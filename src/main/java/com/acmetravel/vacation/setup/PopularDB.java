d5e4254bfa2f265d73d30df8994327dfdc89c15e
Willy <wscatao@gmail.com>
2022-08-02 22:55:06 -0300

    Classe para fazer a leitura do arquivo .json e salvar na base de dados.

---
+++ src/main/java/com/acmetravel/vacation/setup/PopularDB.java
@@ -0,0 +1,38 @@
+package com.acmetravel.vacation.setup;
+
+import com.acmetravel.vacation.domain.Vacation;
+import com.acmetravel.vacation.repository.VacationRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class PopularDB {
+
+    @Autowired
+    private VacationRepository repository;
+
+    public void popular() {
+
+        var objectMapper = new ObjectMapper();
+
+        try {
+
+            var vacations = objectMapper.readValue(new File("src/main/resources/Data/data.json"),
+                    new TypeReference<List<Vacation>>() {
+                    });
+
+            vacations.forEach(vacation -> repository.salvar(vacation));
+
+        } catch (IOException e) {
+
+            throw new RuntimeException(e);
+        }
+    }
+
+}
