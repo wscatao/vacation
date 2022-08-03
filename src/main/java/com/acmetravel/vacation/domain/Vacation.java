34f240fc55936f13f09df02b57a85ba6d78399f9
Willy <wscatao@gmail.com>
2022-08-02 22:54:10 -0300

    Adicionado anotacoes da classe Vacation para mapeamento da base.

--- src/main/java/com/acmetravel/vacation/domain/Vacation.java
+++ src/main/java/com/acmetravel/vacation/domain/Vacation.java
@@ -1,21 +1,53 @@
 package com.acmetravel.vacation.domain;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Document("vacations")
 public class Vacation {
 
-    @Id
-    private String nome;
+    @Indexed(unique = true, name = "indexName")
+    @Field("name")
+    private String name;
 
-    private String descricao;
+    @Field("description")
+    private String description;
+
+    @Field("type")
+    private String type;
+
+    @Field("destinations")
+    private List<Destination> destinations;
+
+    @Field("includes")
+    private List<Include> includes;
+
+    @Field("numberOfNights")
+    private int numberOfNights;
+
+    @Field("pricePP")
+    private BigDecimal pricePP;
+
+    @Field("offer")
+    private Offer offer;
+
+    @Field("validTill")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private LocalDate validTill;
+
+    @Field("soldout")
+    private boolean soldout;
 }
