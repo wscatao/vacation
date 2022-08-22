c6368fcaa67bb13c4a0cc0e5df90df4f59e7ca18
Willy <wscatao@gmail.com>
2022-08-22 08:55:31 -0300

    Removido parametro desnecessario para mapper

--- src/main/java/com/acmetravel/vacation/mapper/VacationDtoMapper.java
+++ src/main/java/com/acmetravel/vacation/mapper/VacationDtoMapper.java
@@ -3,7 +3,6 @@ package com.acmetravel.vacation.mapper;
 import com.acmetravel.vacation.domain.Vacation;
 import com.acmetravel.vacation.dto.VacationDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,6 +10,5 @@ public interface VacationDtoMapper {
 
     VacationDtoMapper INSTANCE = Mappers.getMapper(VacationDtoMapper.class);
 
-    @Mapping(target = "name", source = "name")
     VacationDto vacationToVacationDto(Vacation vacation);
 }
